c4e290643eeff28d5fc6e582cfd3d07a6da44871
Dario <arrieta.dario@hotmail.com>
2021-07-07 10:45:10 -0500

    ordering more files and structure

---
+++ src/App/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
+import ContextProvider from "./components/ContextProvider";
+import Alert from "./components/Alert";
+import Loading from "./components/Loading";
+import Navbar from "./components/Navbar";
+import Routes from "./components/Routes";
+
+function App() {
+	return (
+		<ContextProvider>
+			<BrowserRouter>
+				<div className="flex flex-col h-screen overflow-x-hidden overflow-y-scroll text-secundary bg-primary-dark">
+					<Navbar />
+					<Routes />
+					<Alert/>
+					<Loading/>
+				</div>
+			</BrowserRouter>
+		</ContextProvider>
+	);
+}
+
+export default App;
